e361e85f5b4645a51ce56ed26075ddc786761835
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2024-03-31 22:04:37 +0300

    create the business logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+interface ImageWithData {
+  name: string;
+  barcode: string;
+}
+
+const URL = process.env.REACT_APP_SERVER_URL;
 
 function App() {
+  const [files, setFiles] = useState<any>();
+  const [imagesWithData, setImagesWithData] = useState<ImageWithData[]>([]);
+
+  const handleClick = () => {
+    const formData = new FormData();
+
+    Object.values(files).forEach((file: any) => {
+      formData.append("images", file);
+    });
+
+    axios
+      .post(`${URL}/extract`, formData)
+      .then((response) => {
+        setImagesWithData(response.data.data);
+
+        setFiles([]);
+      })
+      .catch((error) => {
+        console.error("Error uploading PDF:", error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>test</div>
-      </header>
+      <header>Barcode App</header>
+      <div>
+        <input
+          type="file"
+          multiple
+          onChange={(e) => setFiles(e.target.files)}
+        />
+        <button onClick={handleClick}>Click</button>
+
+        <div>
+          {imagesWithData.map((image) => (
+            <div key={image.barcode}>
+              <span>
+                {image.name} - {image.barcode}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
