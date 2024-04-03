dcc4f0c8c2bab2d5acd52587d3a015dc1f882cc0
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2024-04-03 16:07:01 +0300

    redesign the page and separate logic from UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,146 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import axios from "axios";
+import { Button, Card, Flex, Input, message } from "antd";
+import { FileImageOutlined, BarcodeOutlined } from "@ant-design/icons";
+import { SearchProps } from "antd/es/input/Search";
+import { NoticeType } from "antd/es/message/interface";
+import useFetch from "./customHooks/useFetch";
 
 interface ImageWithData {
   name: string;
   barcode: string;
 }
 
-const URL = process.env.REACT_APP_SERVER_URL;
+const { Search } = Input;
 
 function App() {
-  const [files, setFiles] = useState<any>();
-  const [imagesWithData, setImagesWithData] = useState<ImageWithData[]>([]);
+  const {
+    imagesWithData,
+    originalImagesWithData,
+    contextHolder,
+    setImagesWithData,
+    setFiles,
+    handleClick,
+  } = useFetch();
 
-  const handleClick = () => {
-    const formData = new FormData();
+  const onSearch: SearchProps["onSearch"] = (value, _e) => {
+    const filteredImages = originalImagesWithData.filter((image) =>
+      image.name.includes(value)
+    );
 
-    Object.values(files).forEach((file: any) => {
-      formData.append("images", file);
-    });
+    setImagesWithData(filteredImages);
+  };
 
-    axios
-      .post(`${URL}/extract`, formData)
-      .then((response) => {
-        setImagesWithData(response.data.data);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const filteredImages = originalImagesWithData.filter((image) =>
+      image.name.includes(e.target.value)
+    );
 
-        setFiles([]);
-      })
-      .catch((error) => {
-        console.error("Error uploading PDF:", error);
-      });
+    setImagesWithData(filteredImages);
   };
 
   return (
     <div className="App">
-      <header>Barcode App</header>
-      <div>
-        <input
-          type="file"
-          multiple
-          onChange={(e) => setFiles(e.target.files)}
-        />
-        <button onClick={handleClick}>Click</button>
-
-        <div>
-          {imagesWithData.map((image) => (
-            <div key={image.barcode}>
-              <span>
-                {image.name} - {image.barcode}
-              </span>
+      <header className="header">
+        <h1>Barcode App</h1>
+        {contextHolder}
+      </header>
+      <Flex>
+        <Flex vertical align="center" gap={70} className="w-100">
+          <Flex
+            align="center"
+            justify="space-between"
+            gap={30}
+            style={{
+              backgroundColor: "white",
+              marginTop: 20,
+              padding: "30px",
+              borderRadius: "6px",
+            }}
+          >
+            <div className="bg-color-white p-10 border-radius-6">
+              <input
+                className="cursor-pointer"
+                type="file"
+                multiple
+                onChange={(e) => setFiles(e.target.files)}
+              />
             </div>
-          ))}
-        </div>
-      </div>
+
+            <Button onClick={handleClick} type="primary">
+              Upload Files
+            </Button>
+          </Flex>
+
+          <Flex vertical gap={25}>
+            <Flex justify="center">
+              {originalImagesWithData.length > 0 ? (
+                <Search
+                  placeholder="input search text"
+                  onSearch={onSearch}
+                  onChange={(e) => onChange(e)}
+                  style={{ width: 300 }}
+                  size="large"
+                />
+              ) : null}
+            </Flex>
+            <Flex
+              wrap="wrap"
+              gap={40}
+              justify="space-evenly"
+              style={{ margin: 20 }}
+            >
+              {imagesWithData.map((image, index) => (
+                <Card key={index}>
+                  <Flex
+                    vertical
+                    style={{
+                      backgroundColor: "white",
+                      height: 220,
+                      width: 220,
+                    }}
+                    className="h-100 w-100"
+                  >
+                    <Flex
+                      justify="center"
+                      align="center"
+                      className="h-50 w-100"
+                      style={{
+                        borderBottom: "1px solid gray",
+                      }}
+                    >
+                      <div
+                        style={{
+                          borderRadius: "50%",
+                        }}
+                      >
+                        <FileImageOutlined style={{ fontSize: 50 }} />
+                      </div>
+                    </Flex>
+                    <Flex
+                      vertical
+                      align="center"
+                      justify="space-between"
+                      className="h-50"
+                    >
+                      <h5 style={{ fontSize: 16 }}>{image.name}</h5>
+                      <Flex gap={10} align="center">
+                        <BarcodeOutlined />
+                        {image.barcode === "" ? (
+                          <span>Don't found</span>
+                        ) : (
+                          <span>{image.barcode}</span>
+                        )}
+                      </Flex>
+                    </Flex>
+                  </Flex>
+                </Card>
+              ))}
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
     </div>
   );
 }
---
+++ src/customHooks/useFetch.ts
@@ -0,0 +1,70 @@
+import { message } from "antd";
+import { NoticeType } from "antd/es/message/interface";
+import axios from "axios";
+import { useState } from "react";
+
+interface ImageWithData {
+  name: string;
+  barcode: string;
+}
+
+const URL = process.env.REACT_APP_SERVER_URL;
+
+const useFetch = () => {
+  const [files, setFiles] = useState<any>();
+  const [imagesWithData, setImagesWithData] = useState<ImageWithData[]>([]);
+  const [originalImagesWithData, setOriginalImagesWithData] = useState<
+    ImageWithData[]
+  >([]);
+
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const success = (type: NoticeType, content: string) => {
+    messageApi.open({
+      type: type,
+      content: content,
+    });
+  };
+
+  const handleClick = () => {
+    if (files === undefined) {
+      return;
+    }
+    const formData = new FormData();
+
+    Object.values(files).forEach((file: any) => {
+      formData.append("images", file);
+    });
+
+    axios
+      .post(`${URL}/extract`, formData)
+      .then((response) => {
+        setImagesWithData(response.data.data);
+        setOriginalImagesWithData(response.data.data);
+
+        setFiles([]);
+
+        const customMessage = `${response.data.data.length} images were uploaded successfully`;
+
+        success("success", customMessage);
+      })
+      .catch((error) => {
+        console.error("Error uploading PDF:", error);
+        const customMessage = `was not possible to uploaded images`;
+
+        success("error", customMessage);
+      });
+  };
+
+  return {
+    files,
+    imagesWithData,
+    originalImagesWithData,
+    contextHolder,
+    setImagesWithData,
+    setFiles,
+    handleClick,
+  };
+};
+
+export default useFetch;
